7ac12db02d02258ee78849f3734c2ce21bb4da78
Josh Brown <josh@roadfiresoftware.com>
2014-08-08 16:15:24 -0400

    Initial Commit

---
+++ TopMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopMovies
+//
+//  Created by Josh Brown on 8/8/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
