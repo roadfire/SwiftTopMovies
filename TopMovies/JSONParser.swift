59b14623cb48a3188a2f9ea9e28df07f20ccf0c3
Josh Brown <josh@roadfiresoftware.com>
2014-08-28 14:19:59 -0400

    swap good JSON parsing for bad - to demonstrate what's wrong with using valueForKeyPath

--- TopMovies/JSONParser.swift
+++ TopMovies/JSONParser.swift
@@ -12,25 +12,13 @@ class JSONParser {
     func titlesFromJSON(data: NSData) -> [String] {
         var titles = [String]()
         var jsonError: NSError?
+        let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as NSDictionary
         
-        if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary {
-            if let feed = json["feed"] as? NSDictionary {
-                if let entries = feed["entry"] as? NSArray {
-                    for entry in entries {
-                        if let name = entry["im:name"] as? NSDictionary {
-                            if let label = name["label"] as? String {
-                                titles.append(label)
-                            }
-                        }
-                    }
-                }
-            }
+        if let unwrappedError = jsonError {
+            println("json error: \(unwrappedError)")
         } else {
-            if let unwrappedError = jsonError {
-                println("json error: \(unwrappedError)")
-            }
+            titles = json.valueForKeyPath("feed.entry.im:name.label") as [String]
         }
-        
         return titles
     }
 }
