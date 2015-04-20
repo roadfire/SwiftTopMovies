f2713a67a701f19d41b940212a0d64dd3117757d
Josh Brown <josh@roadfiresoftware.com>
2015-04-20 16:45:45 -0400

    updated for Swift 1.2

--- TopMovies/JSONParser.swift
+++ TopMovies/JSONParser.swift
@@ -13,21 +13,19 @@ class JSONParser {
         var titles = [String]()
         var jsonError: NSError?
         
-        if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary {
-            if let feed = json["feed"] as? NSDictionary {
-                if let entries = feed["entry"] as? NSArray {
-                    for entry in entries {
-                        if let name = entry["im:name"] as? NSDictionary {
-                            if let label = name["label"] as? String {
-                                titles.append(label)
-                            }
-                        }
-                    }
+        if let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary,
+            feed = json["feed"] as? NSDictionary,
+            entries = feed["entry"] as? NSArray
+        {
+            for entry in entries {
+                if let name = entry["im:name"] as? NSDictionary,
+                    label = name["label"] as? String {
+                        titles.append(label)
                 }
             }
         } else {
-            if let unwrappedError = jsonError {
-                println("json error: \(unwrappedError)")
+            if let jsonError = jsonError {
+                println("json error: \(jsonError)")
             }
         }
         
